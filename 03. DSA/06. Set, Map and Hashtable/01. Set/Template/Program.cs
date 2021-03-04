98ea3d86b12c069ccdc34dec67060c25762c11a4
boryana.gm <boryana.gm@gmail.com>
2021-03-04 21:28:45 +0200

    Implement HastSet methods

--- 03. DSA/06. Set, Map and Hashtable/01. Set/Template/Program.cs
+++ 03. DSA/06. Set, Map and Hashtable/01. Set/Template/Program.cs
@@ -22,23 +22,80 @@ namespace InHomeActivity.Set
         }
         static bool AreAllElementsUnique<T>(IEnumerable<T> collection)
         {
-            throw new NotImplementedException();
+            HashSet<T> set = new HashSet<T>();
+
+            foreach (var item in collection)
+            {
+                if (!set.Add(item))
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
         static IEnumerable<T> Distinct<T>(IEnumerable<T> collection)
         {
-            throw new NotImplementedException();
+            HashSet<T> set = new HashSet<T>(collection);
+
+            return set;
         }
         static IEnumerable<T> Union<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            HashSet<T> set1 = new HashSet<T>(collection1);
+            HashSet<T> set2 = new HashSet<T>(collection2);
+
+            foreach (var item in set2)
+            {
+                set1.Add(item);
+            }
+
+            return set1;
         }
         static IEnumerable<T> Intersect<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            HashSet<T> set1 = new HashSet<T>(collection1);
+            HashSet<T> set2 = new HashSet<T>(collection2);
+            HashSet<T> intersection = new HashSet<T>();
+
+            foreach (var item in set2)
+            {
+                if (set1.Contains(item))
+                {
+                    intersection.Add(item);
+                }
+            }
+
+            return intersection;
         }
         static IEnumerable<T> Difference<T>(IEnumerable<T> collection1, IEnumerable<T> collection2)
         {
-            throw new NotImplementedException();
+            HashSet<T> set1 = new HashSet<T>(collection1);
+            HashSet<T> set2 = new HashSet<T>(collection2);
+            HashSet<T> differentElements = new HashSet<T>();
+
+            if (set1.Count > set2.Count)
+            {
+                foreach (var item in set1)
+                {
+                    if (!set2.Contains(item))
+                    {
+                        differentElements.Add(item);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var item in set2)
+                {
+                    if (!set1.Contains(item))
+                    {
+                        differentElements.Add(item);
+                    }
+                }
+            }
+
+            return differentElements;
         }
     }
 }
