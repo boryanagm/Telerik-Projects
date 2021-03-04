a69c81f0f5b7e2b4415f2307548ef33a3aa2f817
boryana.gm <boryana.gm@gmail.com>
2021-03-04 17:34:43 +0200

    Find middle node in doubly linked list

---
+++ 03. DSA/04-dsa-practicetasks/DSA-PracticeTasks/04. Find middle node doubly linked/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        LinkedList<int> list = new LinkedList<int>();
+
+        list.AddFirst(5);
+        list.AddFirst(4);
+        list.AddFirst(3);
+        list.AddFirst(7);
+        list.AddFirst(2);
+        list.AddFirst(1);
+
+        LinkedListNode<int> middleNode = list.First;
+        LinkedListNode<int> evenNode = list.First;
+
+        if (list.Count % 2 == 0)
+        {
+            while (evenNode.Next != null)
+            {
+                middleNode = middleNode.Next;
+                evenNode = evenNode.Next.Next;
+                if (evenNode.Next.Next == null)
+                {
+                    break;
+                }
+            }
+        }
+        else
+        {
+            while (evenNode.Next != null)
+            {
+                middleNode = middleNode.Next;
+                evenNode = evenNode.Next.Next;
+            }
+        }
+
+        Console.WriteLine(middleNode.Value);
+        Console.WriteLine(string.Join(" ", list));
+    }
+}
