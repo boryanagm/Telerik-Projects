d55df032c892e417164e299e69ffdeb0d51d1dcf
boryana.gm <boryana.gm@gmail.com>
2021-03-04 17:05:19 +0200

    Test Stack in Main

--- 03. DSA/01. Workshop Stack and Queue/Template/StackQueueWorkshop/Program.cs
+++ 03. DSA/01. Workshop Stack and Queue/Template/StackQueueWorkshop/Program.cs
@@ -7,7 +7,13 @@ namespace Template
     {
         static void Main(string[] args)
         {
-            
+            Stack<int> testStack = new Stack<int>();
+
+            testStack.Push(1);
+            testStack.Push(2);
+            testStack.Push(3);
+
+            Console.WriteLine(string.Join(" ", testStack));
         }
     }
 }
