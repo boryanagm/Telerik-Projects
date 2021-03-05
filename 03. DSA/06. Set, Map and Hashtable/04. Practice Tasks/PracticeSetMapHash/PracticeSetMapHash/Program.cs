797f6d992c5804a4d6842474244860c82c3fba1a
boryana.gm <boryana.gm@gmail.com>
2021-03-05 18:15:43 +0200

    Solve Noah's Ark task

--- 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/PracticeSetMapHash/Program.cs
+++ 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/PracticeSetMapHash/Program.cs
@@ -1,9 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        SortedDictionary<string, int> noahsArk = new SortedDictionary<string, int>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string specie = Console.ReadLine();
+
+            if (!noahsArk.ContainsKey(specie))
+            {
+                noahsArk[specie] = 0;
+            }
+
+            noahsArk[specie]++;
+        }
+
+        string isEven = string.Empty;
+
+        foreach (var kvp in noahsArk)
+        {
+            if (kvp.Value % 2 == 0)
+            {
+                isEven = "Yes";
+            }
+            else
+            {
+                isEven = "No";
+            }
+
+            Console.WriteLine($"{kvp.Key} {kvp.Value} {isEven}");
+        }
     }
 }
