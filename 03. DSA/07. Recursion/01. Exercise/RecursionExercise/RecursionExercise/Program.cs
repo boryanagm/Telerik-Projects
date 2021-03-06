e3886361943861296c8d847a9f2afeb39210f43f
boryana.gm <boryana.gm@gmail.com>
2021-03-06 16:50:36 +0200

    Implement FindSum task

--- 03. DSA/07. Recursion/01. Exercise/RecursionExercise/RecursionExercise/Program.cs
+++ 03. DSA/07. Recursion/01. Exercise/RecursionExercise/RecursionExercise/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main()
     {
+        int n = int.Parse(Console.ReadLine());
 
+        int result = Sum(n);
+
+        Console.WriteLine(result);
+    }
+
+    public static int Sum(int n)
+    {
+        if (n <= 1)
+        {
+            return n;
+        }
+
+        int result = n + Sum(n - 1);
+
+        return result;
     }
 }
