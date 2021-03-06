a35448cfabe4096a4c97f0d446f6423a56baf4e0
boryana.gm <boryana.gm@gmail.com>
2021-03-06 16:16:23 +0200

    Implement Product and ProdictManager class for Inventory Manager

---
+++ 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/02. Inventory Manager V2/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        string input = string.Empty;
+
+        ProductManager.Products = new List<Product>();
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] command = input.Split(' ');
+
+            switch (command[0])
+            {
+                case "add":
+
+                    string name = command[1];
+                    float price = float.Parse(command[2]);
+                    string type = command[3];
+
+                    if (ProductManager.IsProductExisting(name))
+                    {
+                        Console.WriteLine($"Error: Item {name} already exists");
+                        break;
+                    }
+                    else
+                    {
+                        ProductManager.AddProduct(name, price, type);
+                        Console.WriteLine($"Ok: Item {name} added successfully");
+                    }
+
+                    break;
+
+                case "filter":
+
+                    string filterBy = command[2];
+
+                    if (filterBy == "price")
+                    {
+                        if (command[3] == "from" && command.Length == 5)
+                        {
+                            if ((ProductManager.GetProductsAboveMinPrice(float.Parse(command[4]))).Count > 0)
+                            {
+                                ProductManager.PrintItems(ProductManager.GetProductsAboveMinPrice(float.Parse(command[4])));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ok:");
+                            }
+                        }
+                        else if (command[3] == "to")
+                        {
+
+                            if ((ProductManager.GetProductsUnderMaxPrice(float.Parse(command[4]))).Count > 0)
+                            {
+                                ProductManager.PrintItems(ProductManager.GetProductsUnderMaxPrice(float.Parse(command[4])));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ok:");
+                            }
+                        }
+                        else if (command.Length == 7)
+                        {
+
+                            if ((ProductManager.GetProductsBetweenPrices(float.Parse(command[4]), float.Parse(command[6]))).Count > 0)
+                            {
+                                ProductManager.PrintItems(ProductManager.GetProductsBetweenPrices(float.Parse(command[4]), float.Parse(command[6])));
+                            }
+                            else
+                            {
+                                Console.WriteLine("Ok:");
+                            }
+                        }
+                    }
+                    else if (filterBy == "type")
+                    {
+                        if (ProductManager.DoesTypeExist(command[3]))
+                        {
+                            ProductManager.PrintItems(ProductManager.GetProductsByType(command[3]));
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Error: Type {command[3]} does not exists");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+public class Product
+{
+    public Product(string name, float price, string type)
+    {
+        this.Name = name;
+        this.Price = price;
+        this.Type = type;
+    }
+
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public string Type { get; set; }
+}
+
+public class ProductManager
+{
+    public static List<Product> Products { get; set; }
+
+    public static bool DoesTypeExist(string type)
+    {
+        return Products.Any(x => x.Type == type);
+    }
+
+    public static bool IsProductExisting(string name)
+    {
+        return Products.Any(x => x.Name == name);
+    }
+
+    public static void AddProduct(string name, float price, string type)
+    {
+        Products.Add(new Product(name, price, type));
+    }
+
+    public static List<Product> GetProductsByType(string type)
+    {
+        return Products.Where(x => x.Type == type).OrderBy(x => x.Price).Take(10).ToList();
+    }
+
+    public static List<Product> GetProductsUnderMaxPrice(float price)
+    {
+        return Products.Where(x => x.Price <= price).OrderBy(x => x.Price).ThenBy(x => x.Name).ThenBy(x => x.Type).Take(10).ToList();
+    }
+
+    public static List<Product> GetProductsAboveMinPrice(float price)
+    {
+        return Products.Where(x => x.Price >= price).OrderBy(x => x.Price).ThenBy(x => x.Name).ThenBy(x => x.Type).Take(10).ToList();
+    }
+
+    public static List<Product> GetProductsBetweenPrices(float price1, float price2)
+    {
+        return Products.Where(x => x.Price >= price1 && x.Price <= price2).OrderBy(x => x.Price).ThenBy(x => x.Type).ThenBy(x => x.Name).Take(10).ToList();
+    }
+
+    public static void PrintItems(List<Product> ordered)
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.Append("Ok: ");
+
+        string separator = ", ";
+
+        if (ordered != null)
+        {
+            foreach (var item in ordered)
+            {
+                sb.Append($"{item.Name}({item.Price})");
+                sb.Append(separator);
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", sb.ToString().TrimEnd(',', ' ')));
+    }
+}
--- 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/02. Inventory Manager/Program.cs
+++ 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/02. Inventory Manager/Program.cs
@@ -1,9 +1,83 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main()
     {
+        string input = string.Empty;
 
+        Dictionary<string, double> itemPricePair = new Dictionary<string, double>();
+        Dictionary<string, string> itemTypePair = new Dictionary<string, string>();
+
+        while ((input = Console.ReadLine()) != "end")
+        {
+            string[] command = input.Split(' ');
+
+            switch (command[0])
+            {
+                case "add":
+
+                    string name = command[1];
+                    double price = double.Parse(command[2]);
+                    string type = command[3];
+
+                    if (itemPricePair.ContainsKey(name))
+                    {
+                        Console.WriteLine($"Error: Item {name} already exists");
+                        break;
+                    }
+                    else
+                    {
+                        itemPricePair[name] = price;
+                        itemTypePair[name] = type;
+
+                        Console.WriteLine($"Ok: Item {name} added successfully");
+                    }
+
+                    break;
+
+                case "filter":
+
+                    string filterBy = command[2];
+
+                    if (filterBy == "name")
+                    {
+
+                    }
+                    else if (filterBy == "price")
+                    {
+
+                    }
+                    else if (filterBy == "type")
+                    {
+                        if (!itemTypePair.ContainsValue(command[3]))
+                        {
+                            Console.WriteLine($"Error: Type {command[3]} does not exists");
+                            break;
+                        }
+                        else
+                        {
+                            int counter = 0;
+
+                            foreach (var kvp in itemPricePair.OrderBy(x => x.Value))
+                            {
+                                if (itemTypePair.ContainsValue(command[3]))
+                                {
+                                    Console.Write($"{kvp.Key}({kvp.Value})");
+                                    counter++;
+                                }
+                               
+                                if (counter == 10)
+                                {
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
     }
 }
