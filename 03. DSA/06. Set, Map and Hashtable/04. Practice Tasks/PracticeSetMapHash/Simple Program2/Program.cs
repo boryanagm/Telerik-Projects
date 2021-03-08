912ed9d8156815d6509e85660defbe9d1d0467f6
boryana.gm <boryana.gm@gmail.com>
2021-03-08 12:47:14 +0200

    Test Simple program

---
+++ 03. DSA/06. Set, Map and Hashtable/04. Practice Tasks/PracticeSetMapHash/Simple Program2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string first =  "look for repeating strings in this sentence" ;
+        string second =  "look for strings" ;
+
+        string[] arr = second.Split(' ');
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (!first.Contains(arr[i]))
+            {
+                Console.WriteLine("No");
+                return;
+            }
+
+            first = first.Replace(arr[i], " "); // will remove all the occurencies
+        }
+
+        Console.WriteLine("Yes");
+    }
+}
