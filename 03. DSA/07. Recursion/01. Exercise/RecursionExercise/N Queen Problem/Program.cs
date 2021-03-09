bba6b4607c255704d4354f98d812b22d803b5280
boryana.gm <boryana.gm@gmail.com>
2021-03-09 16:51:38 +0200

    Implement Fast Recursive Fibonacci

---
+++ 03. DSA/07. Recursion/01. Exercise/RecursionExercise/N Queen Problem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Numerics;
+
+class Program
+{
+    static void Main()
+    {
+        var memo = new BigInteger[100]; // The max length of the array would be the max Fibonacci number
+        Console.WriteLine(Fibonacci(50, memo));
+    }
+
+    static BigInteger Fibonacci(int n, BigInteger[] memo)
+    {
+        if ((n == 1) || (n == 2))
+        {
+            return 1;
+        }
+        else
+        {
+            if (memo[n] == 0)
+            {
+                memo[n] = Fibonacci(n - 1, memo) + Fibonacci(n - 2, memo);
+            }
+            return memo[n];
+        }
+    }
+}
