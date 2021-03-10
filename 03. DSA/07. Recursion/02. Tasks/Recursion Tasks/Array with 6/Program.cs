28fc6dac0de233bf7edfa738c3dac3413f35ee6e
boryana.gm <boryana.gm@gmail.com>
2021-03-10 20:05:38 +0200

    Implement Array with 6

---
+++ 03. DSA/07. Recursion/02. Tasks/Recursion Tasks/Array with 6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+        int index = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(FindSix(arr, index).ToString().ToLower());
+    }
+
+    static bool FindSix(int[] arr, int index)
+    {
+        if (index == arr.Length - 1)
+        {
+            if (arr[index] == 6)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        if (arr[index] != 6)
+        {
+           return FindSix(arr, index + 1);
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
