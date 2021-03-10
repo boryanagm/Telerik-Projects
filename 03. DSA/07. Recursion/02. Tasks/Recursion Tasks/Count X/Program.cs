191d80b57acd5fe41634d57a2c4f21477acd077e
boryana.gm <boryana.gm@gmail.com>
2021-03-10 18:01:59 +0200

    Implement CountX

---
+++ 03. DSA/07. Recursion/02. Tasks/Recursion Tasks/Count X/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string x = Console.ReadLine();
+
+        Console.WriteLine(CountX(x, x.Length - 1));
+    }
+
+    static int CountX(string x, int currentIndex)
+    {
+        if (currentIndex == 0)
+        {
+            if (x[currentIndex] == 'x')
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        if (x[currentIndex] == 'x')
+        {
+            return CountX(x, currentIndex - 1) + 1;
+        }
+
+        return CountX(x, currentIndex - 1);
+    }
+}
