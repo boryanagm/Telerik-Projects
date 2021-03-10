e5ef4eada6710f3d248ae24769b80b798f395b88
boryana.gm <boryana.gm@gmail.com>
2021-03-10 20:13:15 +0200

    Implement Arrays containing 11

---
+++ 03. DSA/07. Recursion/02. Tasks/Recursion Tasks/Arrays containing 11/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] arr = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+
+        int index = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(CountElevens(arr, index));
+    }
+
+    static int CountElevens(int[] arr, int index)
+    {
+        if (index == arr.Length) 
+        {
+            return 0;
+        }
+
+        if (arr[index] == 11)
+        {
+            return CountElevens(arr, index + 1) + 1;
+        }
+        else
+        {
+            return CountElevens(arr, index + 1);
+        }
+    }
+}
