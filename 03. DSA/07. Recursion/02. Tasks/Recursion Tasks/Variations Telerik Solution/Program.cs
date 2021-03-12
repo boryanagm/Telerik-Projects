f7e2145070404fe3560f8f485eeff889e9cbd35a
boryana.gm <boryana.gm@gmail.com>
2021-03-12 16:47:50 +0200

    Add Variations Telerik Solution

---
+++ 03. DSA/07. Recursion/02. Tasks/Recursion Tasks/Variations Telerik Solution/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static List<string> variations = new List<string>();
+
+    static void Main(string[] args)
+    {
+        int length = int.Parse(Console.ReadLine());
+
+        List<char> symbols = Console.ReadLine().Split().Select(x => x[0]).ToList();
+
+        symbols.Sort();
+
+        char firstSymbol = symbols[0];
+        char secondSymbol = symbols[1];
+
+        GetVariations(firstSymbol, secondSymbol, length, "");
+
+        Console.WriteLine(String.Join($"{Environment.NewLine}", variations));
+    }
+
+    static void GetVariations(char firstSymbol, char secondSymbol, int length, string current)
+    {
+        if (current.Length == length)
+        {
+            variations.Add(current);
+            return;
+        }
+
+        GetVariations(firstSymbol, secondSymbol, length, current + firstSymbol);
+        GetVariations(firstSymbol, secondSymbol, length, current + secondSymbol);
+    }
+}
