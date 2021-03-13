4af7acc61431eb723fa8ac65773a8b52d4e3963b
boryana.gm <boryana.gm@gmail.com>
2021-03-13 18:16:44 +0200

    Implement InFlight entertainment solution

---
+++ 03. DSA/08. Searching and Sorting/Searching and Sorting Exercise/Inflight Entertainment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int flightLength = int.Parse(Console.ReadLine());
+
+        int[] moviesLengths = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+        bool areThereTwoMovies = AreThereTwoMovies(flightLength, moviesLengths);
+
+        Console.WriteLine(areThereTwoMovies);
+    }
+    // My solution assuming that there are movies with the same length
+
+    static bool AreThereTwoMovies(int flightLength, int[] moviesLengths)
+    {
+        for (int i = 0; i < moviesLengths.Length; i++)
+        {
+            int diff = flightLength - moviesLengths[i];
+            List<int> current = moviesLengths.ToList();
+            current.RemoveAt(i);
+
+            if (current.Contains(diff))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
