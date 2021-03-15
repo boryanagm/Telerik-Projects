a38398c3404b851d2043962128fa3e99b61d3d84
boryana.gm <boryana.gm@gmail.com>
2021-03-15 23:52:45 +0200

    Delete node class

---
+++ 03. DSA/09. Trees/Workshop Binary Search Tree/Template/BST/BinarySearchNode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BST
+{
+    class BinarySearchNode<T>
+    {
+        public BinarySearchNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value
+        {
+            get;
+        }
+
+        public BinarySearchNode<T> Left
+        {
+            get;
+            set;
+        }
+
+        public BinarySearchNode<T> Right
+        {
+            get;
+            set;
+        }
+    }
+}
