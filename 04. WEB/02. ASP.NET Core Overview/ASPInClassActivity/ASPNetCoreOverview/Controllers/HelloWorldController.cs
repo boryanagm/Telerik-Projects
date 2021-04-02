509eac64d283c478025bef933cfcce9ce4416017
boryana.gm <boryana.gm@gmail.com>
2021-04-02 11:56:49 +0300

    add in class activity demo

---
+++ 04. WEB/02. ASP.NET Core Overview/ASPInClassActivity/ASPNetCoreOverview/Controllers/HelloWorldController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreOverview.Controllers
+{
+	[Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        public string SayHello()
+        {
+            return "Hello, World!";
+        }
+    }
+}
---
+++ 04. WEB/02. ASP.NET Core Overview/ASPInClassActivity/ASPNetCoreOverview/Models/Beer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ASPNetCoreOverview.Models
+{
+	public class Beer
+    {
+        public int Id { get; set; }
+       
+        [StringLength(25, MinimumLength = 4, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public string Name { get; set; }
+
+        [Range(0.1, 35.00, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public double Abv { get; set; }
+    }
+}
