0a09812fe458e19e63155ddaf72a288319c43078
boryana.gm <boryana.gm@gmail.com>
2021-04-02 15:51:52 +0300

    Add UserController

---
+++ 04. WEB/02. ASP.NET Core Overview/ASPInClassActivity/ASPNetCoreOverview/Controllers/UserController.cs
@@ -0,0 +1,105 @@
+﻿using ASPNetCoreOverview.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreOverview.Controllers
+{
+    [ApiController]
+    [Route("/api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly List<User> users = new List<User>
+        {
+            new User
+            {
+                Id = 1,
+                Name = "Stamat",
+                Email = "stam@gmail.com"
+            },
+            new User
+            {
+                Id = 2,
+                Name = "Begonia",
+                Email = "rosna.kitka@abv.bg"
+            },
+        };
+
+        //[HttpGet("")]
+        //public IActionResult Get()
+        //{
+        //    return this.Ok(this.users);
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = this.users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(user);
+        }
+        [HttpPost("")]
+        public IActionResult Post([FromBody] User model)
+        {
+            if (model == null)
+            {
+                return this.BadRequest();
+            }
+
+            this.users.Add(model);
+
+            return this.Created("post", model);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User model)
+        {
+            if (id < 1 || model == null)
+            {
+                return this.BadRequest();
+            }
+
+            var user = users.FirstOrDefault(u => u.Id == id);
+
+            user.Name = model.Name;
+            user.Email = model.Email;
+
+            return this.Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var user = users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return this.NotFound();
+            }
+
+            this.users.Remove(user);
+
+            return this.NoContent();
+        }
+
+        [HttpGet("")]
+        public IActionResult Order([FromQuery] string order)
+        {
+            if (order == "desc")
+            {
+                return this.Ok(users.OrderByDescending(u => u.Id));
+            }
+            else
+            {
+                return this.Ok(users.OrderBy(u => u.Id));
+            }
+        }
+    }
+}
---
+++ 04. WEB/02. ASP.NET Core Overview/ASPInClassActivity/ASPNetCoreOverview/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreOverview.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [MaxLength(50)]
+        [RegularExpression(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Please enter correct email.")]
+        public string Email { get; set; }
+
+    }
+}
