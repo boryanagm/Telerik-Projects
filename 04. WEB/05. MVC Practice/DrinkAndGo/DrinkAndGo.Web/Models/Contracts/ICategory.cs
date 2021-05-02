0a7259693c3560f1fba8e4825f595428be6b80c8
boryana.gm <boryana.gm@gmail.com>
2021-05-02 21:05:31 +0300

    Cerate ICategory Interface

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/Contracts/ICategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Web.Models.Contracts
+{
+    public interface ICategory
+    {
+        ICollection<Category> Categories { get; }
+    }
+}
