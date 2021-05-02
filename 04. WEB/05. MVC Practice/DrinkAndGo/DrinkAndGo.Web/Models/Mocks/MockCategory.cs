12369ce3a1b7ec53ba24aed6063737ed181a06c1
boryana.gm <boryana.gm@gmail.com>
2021-05-02 21:10:07 +0300

    Create MockCategory

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/Mocks/MockCategory.cs
@@ -0,0 +1,24 @@
+﻿using DrinkAndGo.Web.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Web.Models.Mocks
+{
+    public class MockCategory : ICategory
+    {
+        public ICollection<Category> Categories => new List<Category>
+        {
+          new Category
+          {
+           Name = "Alcoholic",
+           Description = "All alcoholic drinks"
+          },
+          new Category
+          {
+           Name = "Non-alcoholic",
+           Description = "All non-alcoholic drinks"
+          }};
+    }
+}
