58ea93222b4af1d50a2c857294451af7e2f2d1ee
boryana.gm <boryana.gm@gmail.com>
2021-05-04 12:24:17 +0300

    Create AddToCart method in ShoppingCart class

--- 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCart.cs
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCart.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DrinkAndGo.Web.Models
 {
@@ -14,7 +15,7 @@ namespace DrinkAndGo.Web.Models
         {
             this.context = context;
         }
-        public string Id { get; set; }
+        public string ShoppingCartId { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
 
         public static ShoppingCart GetCart(IServiceProvider services)
@@ -29,8 +30,32 @@ namespace DrinkAndGo.Web.Models
 
             return new ShoppingCart(context)
             {
-                Id = id
+                ShoppingCartId = id
             };
+        }
+
+        public void AddToCart(Drink drink, int amount)
+        {
+            var shoppingCartItem = this.context.ShoppingCartItems
+                .SingleOrDefault(s => s.Drink.Id == drink.Id && s.ShoppingCartId == ShoppingCartId);
+
+            if (shoppingCartItem == null)
+            {
+                shoppingCartItem = new ShoppingCartItem
+                {
+                    ShoppingCartId = ShoppingCartId,
+                    Drink = drink,
+                    Amount = 1
+                };
+
+                this.context.ShoppingCartItems.Add(shoppingCartItem);
+            }
+            else
+            {
+                shoppingCartItem.Amount++;
+            }
+
+            this.context.SaveChanges();
         }
     }
 }
--- 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCartItem.cs
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCartItem.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DrinkAndGo.Web.Models
+﻿namespace DrinkAndGo.Web.Models
 {
     public class ShoppingCartItem
     {
         public int Id { get; set; }
         public Drink Drink { get; set; }
         public int Amount { get; set; }
-        public string ShoppingCardId {get;set;}
+        public string ShoppingCartId {get;set;}
     }
 }
