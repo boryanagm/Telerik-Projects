f94a572d6ebb7468e256f144cf22f2159b1bc9ec
boryana.gm <boryana.gm@gmail.com>
2021-05-04 17:56:08 +0300

    Add ShoppingCart button to home page

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Components/ShoppingCartSummary.cs
@@ -0,0 +1,31 @@
+﻿using DrinkAndGo.Web.Models;
+using DrinkAndGo.Web.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DrinkAndGo.Web.Components
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+        private readonly ShoppingCart shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            this.shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = new List<ShoppingCartItem>() { new ShoppingCartItem(), new ShoppingCartItem()}; // this.shoppingCart.GetShoppingCartItems();
+            this.shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = this.shoppingCart,
+                ShoppingCartTotal = this.shoppingCart.GetShoppingCartTotal()
+            };
+
+            return View(shoppingCartViewModel);
+        }
+    }
+}
