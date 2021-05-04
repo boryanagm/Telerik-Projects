4274457d87b00a48eb9b211482481f95d85011f9
boryana.gm <boryana.gm@gmail.com>
2021-05-04 20:59:12 +0300

    Create CategoryMenu component

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Components/CategoryMenu.cs
@@ -0,0 +1,27 @@
+﻿using DrinkAndGo.Web.Models.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Web.Components
+{
+    public class CategoryMenu : ViewComponent
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoryMenu(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = this.categoryRepository.Categories
+                .OrderBy(c => c.Name);
+
+            return View(categories);
+        }
+    }
+}
