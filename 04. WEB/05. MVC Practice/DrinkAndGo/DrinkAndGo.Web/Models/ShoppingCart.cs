20e5fa4cd95e9b9875a78225476611f68772e84b
boryana.gm <boryana.gm@gmail.com>
2021-05-04 12:12:47 +0300

    Create GetCart method in ShoppingCart class

--- 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCart.cs
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/ShoppingCart.cs
@@ -1,14 +1,36 @@
-﻿using DrinkAndGo.Web.Migrations;
+﻿using DrinkAndGo.Web.Database;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace DrinkAndGo.Web.Models
 {
     public class ShoppingCart
     {
-        public int Id { get; set; }
+        private readonly DrinkAndGoDbContext context;
+
+        public ShoppingCart(DrinkAndGoDbContext context)
+        {
+            this.context = context;
+        }
+        public string Id { get; set; }
         public List<ShoppingCartItem> ShoppingCartItems { get; set; }
+
+        public static ShoppingCart GetCart(IServiceProvider services)
+        {
+            ISession session = services.GetRequiredService<IHttpContextAccessor>()?
+                .HttpContext.Session;
+
+            var context = services.GetService<DrinkAndGoDbContext>();
+
+            string id = session.GetString("Id") ?? Guid.NewGuid().ToString();
+            session.SetString("Id", id);
+
+            return new ShoppingCart(context)
+            {
+                Id = id
+            };
+        }
     }
 }
