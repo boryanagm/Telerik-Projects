e1b062633003c147e40e54afb1a8c57eb6377cfe
boryana.gm <boryana.gm@gmail.com>
2021-05-05 17:55:04 +0300

    Create OrdersController and Checkout view

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Controllers/OrdersController.cs
@@ -0,0 +1,26 @@
+﻿using DrinkAndGo.Web.Models;
+using DrinkAndGo.Web.Models.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Web.Controllers
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrdersController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+        }
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+    }
+}
