e4b5fec3adc79d413f00ac8b8c4e71eb919479ea
boryana.gm <boryana.gm@gmail.com>
2021-05-05 14:38:03 +0300

    Create Order and OrderDetail classes

---
+++ 04. WEB/05. MVC Practice/DrinkAndGo/DrinkAndGo.Web/Models/OrderDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrinkAndGo.Web.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+
+        public int OrderId { get; set; }
+        public virtual Order Order { get;set;}
+
+        public int DrinkId { get; set; }
+        public virtual Drink Drink { get; set; }
+            
+        public int Amount { get; set; }
+        public decimal Price { get; set; }
+    }
+}
